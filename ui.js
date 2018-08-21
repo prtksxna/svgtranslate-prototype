3cd0fcec44bf83652cdde8f08ecf0ac831d4fac7
Prateek Saxena <prtksxna@gmail.com>
2018-08-21 13:18:35 +0530

    feat: login

--- ui.js
+++ ui.js
@@ -1,3 +1,4 @@
+// Select file form
 var form = new OO.ui.ActionFieldLayout(
   new OO.ui.TextInputWidget(),
   new OO.ui.ButtonWidget( {
@@ -9,5 +10,32 @@ var form = new OO.ui.ActionFieldLayout(
     label: 'Select file to translate',
     help: 'Type a file name from Commons',
 } )
-
 $( '#select-form' ).append( form.$element );
+
+// Login
+showAccountMenu = function () {
+  $logout = $( '<a>' ).text( 'Logout (TestUser)').attr( 'href', '#' );
+  $lang = $( '<a>' ).text( 'Language settings').attr( 'href', '#' );
+  $('nav').empty().append(
+    $lang,
+    '&nbsp;&middot;&nbsp;',
+    $logout
+  );
+
+  $logout.on( 'click', function () {
+    window.localStorage.setItem( 'logged', 'out' );
+    window.location = window.location.pathname;
+  })
+}
+
+$( '#login' ).on( 'click', function () {
+  var menu = $( this ).parent();
+  OO.ui.confirm( 'You will be redirected to Commons in order to confirm your indentity. Montage will not publish anything on Wikimedia projects using your account.' ).done( function () {
+    window.localStorage.setItem( 'logged', 'in' );
+    showAccountMenu();
+  } );
+} );
+
+if ( window.localStorage.getItem( 'logged' ) === 'in' ) {
+  showAccountMenu();
+}
