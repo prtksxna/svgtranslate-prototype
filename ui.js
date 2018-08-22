334f60fa419772c4ffabf5bcd1b50a71ab804ba2
Prateek Saxena <prtksxna@gmail.com>
2018-08-22 07:29:55 +0530

    feat: add language settings

--- ui.js
+++ ui.js
@@ -25,7 +25,11 @@ showAccountMenu = function () {
   $logout.on( 'click', function () {
     window.localStorage.setItem( 'logged', 'out' );
     window.location = window.location.pathname;
-  })
+  } );
+
+  $lang.on( 'click', function () {
+    openLangSettings();
+  } );
 }
 
 $( '#login' ).on( 'click', function () {
@@ -38,4 +42,110 @@ $( '#login' ).on( 'click', function () {
 
 if ( window.localStorage.getItem( 'logged' ) === 'in' ) {
   showAccountMenu();
+}
+
+// Language settings
+function SettingsDialog( config ) {
+    SettingsDialog.parent.call( this, config );
+}
+OO.inheritClass( SettingsDialog, OO.ui.ProcessDialog );
+
+SettingsDialog.static.name = 'settingsDialog';
+SettingsDialog.static.title = 'Settings';
+SettingsDialog.static.actions = [
+    { action: 'save', label: 'Done', flags: [ 'primary', 'progressive' ]},
+    { label: 'Cancel', flags: 'safe' }
+];
+
+SettingsDialog.prototype.initialize = function () {
+    SettingsDialog.parent.prototype.initialize.apply( this, arguments );
+    this.content = new OO.ui.PanelLayout( { padded: true, expanded: false } );
+    this.content.$element.append(
+      this.getSettingsElement(),
+      '<br><br><br><br><br>'
+    );
+    this.$body.append( this.content.$element );
+};
+
+SettingsDialog.prototype.getSettingsElement = function () {
+  // Set default
+  if ( window.localStorage.getItem('langs') === null ) {
+    var def = [
+      {
+        data: 'hi',
+        label: 'Hindi'
+      },
+      {
+        data: 'en',
+        label: 'English'
+      }
+    ];
+    window.localStorage.setItem( 'langs', JSON.stringify( def ) );
+  }
+
+  var langSelector = new OO.ui.MenuTagMultiselectWidget( {
+    selected: JSON.parse( window.localStorage.getItem( 'langs' ) ),
+    options: [
+      {
+        data: 'hi',
+        label: 'Hindi'
+      },
+      {
+        data: 'en',
+        label: 'English'
+      },
+      {
+        data: 'ur',
+        label: 'Urdu',
+      },
+      {
+        data: 'it',
+        label: 'Italian'
+      },
+      {
+        data: 'ja',
+        label: 'Japanese'
+      }
+    ]
+  } );
+
+  langSelector.on( 'change', function ( items ) {
+    var selected = items.map( function ( item ) {
+      return {
+        data: item.data,
+        label: item.label
+      };
+    })
+    window.localStorage.setItem( 'langs', JSON.stringify(selected ) );
+  });
+
+  var field = new OO.ui.FieldLayout(
+    langSelector, {
+      align: 'top',
+      label: 'Preferred languages',
+      help: "These languages are show near the top when you're switching between them in the translate page",
+      helpInline: true
+    }
+  )
+  return field.$element;
+}
+
+SettingsDialog.prototype.getActionProcess = function ( action ) {
+    var dialog = this;
+    if ( action ) {
+        return new OO.ui.Process( function () {
+            dialog.close( { action: action } );
+        } );
+    }
+    return SettingsDialog.parent.prototype.getActionProcess.call( this, action );
+};
+
+var windowManager = new OO.ui.WindowManager();
+$( 'body' ).append( windowManager.$element );
+
+var dialog = new SettingsDialog();
+windowManager.addWindows( [ dialog ] );
+
+function openLangSettings () {
+  windowManager.openWindow( dialog );
 }
